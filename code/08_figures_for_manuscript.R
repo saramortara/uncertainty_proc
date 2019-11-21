05bccdd5b0f0c2dc37c0460bbf08068da47c1547
saramortara <saramortara@gmail.com>
2019-11-21 11:26:13 -0300

    adding code for figure 3

--- code/08_figures_for_manuscript.R
+++ code/08_figures_for_manuscript.R
@@ -2,6 +2,7 @@
 
 # loading packages
 library("ggplot2")
+library("ggpubr")
 # loading functions
 source("R/sun_plot.R")
 source('R/read_sun.R')
@@ -109,10 +110,68 @@ for (i in 1:length(count)) {
   file.f2[[i]] <- file[[i]][count[i]]
 }
 
-file.f2[[1]]
+f2a <- sun_plot(as.data.frame(file.f2[[1]]), title = "")
+f2b <- sun_plot(as.data.frame(file.f2[[2]]), title = "")
+f2c <- sun_plot(as.data.frame(file.f2[[3]]), title = "")
+f2d <- sun_plot(as.data.frame(file.f2[[4]]), title = "")
+f2e <- sun_plot(as.data.frame(file.f2[[5]]), title = "")
+f2f <- sun_plot(as.data.frame(file.f2[[6]]), title = "")
 
-sun_plot(as.data.frame(file.f2[[1]]), title = "")
 
-f2a
+f2.all <- ggarrange(f2a + labs(x = "", y = "") + ggtitle("A"),
+                    f2b + labs(x = "", y = "") + ggtitle("B"),
+                    f2c + labs(x = "", y = "") + ggtitle("C"),
+                    f2d + labs(x = "", y = "") + ggtitle("D"),
+                    f2e + labs(x = "", y = "") + ggtitle("E"),
+                    f2f + labs(x = "", y = "") + ggtitle("F"),
+                    common.legend = TRUE)
 
-sun_plot
+png("figs/figure02.png", res = 300, height = 1800, width = 2400)
+annotate_figure(f2.all,
+                left = "Uncertainty",
+                bottom = "Suitability")
+dev.off()
+
+#### Figure 3. Uncertainty metrics ~ N and algorithm ####
+stats_rm <- read.csv("results/final_tables/stats_row_mean.csv")
+stats_rm$size <- as.factor(stats_rm$size)
+stats_rm$sp.gen <- paste(stats_rm$genus, stats_rm$epithet, sep = "_")
+stats_rm$good_bad <- stats_rm$good + stats_rm$bad
+
+# selecting only no clump data
+stats_rm <- stats_rm[stats_rm$clump == "N",]
+
+# color palette
+# color palette
+n.col <- 4
+pal <-  wesanderson::wes_palette("Zissou1", n.col,
+                    type = "continuous")
+
+# algorithm names
+algo_names <- c("Bioclim", "GLM", "Maxent", "Random Forest", "SVM")
+
+
+head(stats_rm)
+
+m1 <- ggplot(stats_rm, aes(x = algorithm, y = M1, fill = size)) +
+  geom_boxplot() +
+  labs(y = "Un_total", x = "Algorithm") +
+  #facet_grid(clump ~ .) +
+  scale_fill_manual(values = pal) +
+  scale_x_discrete(labels = algo_names) +
+  theme_classic()
+
+m1
+
+m2 <- m1 %+% aes(y = M3) +
+  labs(y = "Un_partial")
+
+m2
+
+f3 <- ggarrange(m1 + ggtitle("A"),
+                m2 + ggtitle("B"),
+                common.legend = TRUE)
+
+png("figs/figure03.png", res = 300, height = 1200, width = 2400)
+f3
+dev.off()
